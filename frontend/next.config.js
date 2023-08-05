44bc74cdf42e96ac996e5b7da303754b1a5e8da2
Kenji Saito <ken-yo@mbr.nifty.com>
2023-08-05 19:28:12 +0900

    Fix the errors

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,8 +7,13 @@ const config = {
   images: {
     unoptimized: true,
   },
-  experimental: {
-    //   swcPlugins: [['typewind/swc', {}]],
+  webpack: (config, { webpack, isServer, nextRuntime }) => {
+    // Avoid AWS SDK Node.js require issue
+    if (isServer && nextRuntime === "nodejs")
+      config.plugins.push(
+        new webpack.IgnorePlugin({ resourceRegExp: /^(aws-crt|@aws-sdk\/signature-v4-crt)$/ })
+      );
+    return config;
   },
 };
 
