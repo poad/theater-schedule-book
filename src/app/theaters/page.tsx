c55e13f4c6b27e03bcbb3000e0c56f147f6ca75b
Kenji Saito <ken-yo@mbr.nifty.com>
2023-08-10 21:26:23 +0900

    Bumps node modules

--- src/app/theaters/page.tsx
+++ src/app/theaters/page.tsx
@@ -1,17 +1,45 @@
 'use client';
-import { createClient } from '@supabase/supabase-js';
+import { useState } from 'react';
+import { useSupabase } from '../supabase';
+import { Input, Button } from '@supabase/ui';
 
-export default async function Index(): Promise<JSX.Element> {
-  const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
-  );
+export default function Index(): JSX.Element {
+  const supabase = useSupabase();
+  const [name, setName] = useState<string>();
+  const [theaters, setTheaters] = useState<{ id: string; name: string }[]>();
 
-  const { data: theaters } = await supabase.from('theaters').select();
+  supabase
+    .from('theaters')
+    .select<'*', { id: string; name: string }>()
+    .then(({ data }) => setTheaters(data ?? []));
+
+  function handleClick() {
+    if (name && name.length > 0) {
+      supabase
+        .from('theaters')
+        .insert(name)
+        .select<'*', { id: string; name: string }>()
+        .then(({ data }) => setTheaters(data ?? []));
+    }
+  }
 
   return (
-    <ul className="my-auto">
-      {theaters?.map((theater) => <li key={theater.id}>{theater.name}</li>)}
-    </ul>
+    <>
+      <div>
+        <ul className="my-auto">
+          {theaters?.map((theater) => <li key={theater.id}>{theater.name}</li>)}
+        </ul>
+      </div>
+      <div>
+        <Input
+          label="Teater name"
+          placeholder="name of theater to add"
+          onChange={(event) => setName(event.target.value)}
+        />
+      </div>
+      <div>
+        <Button onClick={() => handleClick()}>Save</Button>
+      </div>
+    </>
   );
 }
--- supabase/migrations/20230618024722_init.sql
+++ supabase/migrations/20230618024722_init.sql
@@ -1,18 +1,16 @@
-create table if not exists todos (
+create table if not exists theaters (
   id uuid default gen_random_uuid() primary key,
-  created_at timestamp with time zone default timezone('utc'::text, now()) not null,
-  title text,
-  is_complete boolean default false,
+  name text not null,
   user_id uuid references auth.users default auth.uid()
 );
 
 -- Set up Row Level Security (RLS)
 -- See https://supabase.com/docs/guides/auth/row-level-security for more details.
-alter table todos
+alter table theaters
   enable row level security;
 
-create policy "Authenticated users can select todos" on todos
+create policy "Authenticated users can select theaters" on theaters
   for select to authenticated using (true);
 
-create policy "Authenticated users can insert their own todos" on todos
+create policy "Authenticated users can insert their own theaters" on theaters
   for insert to authenticated with check (auth.uid() = user_id);
