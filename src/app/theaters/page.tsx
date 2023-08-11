cb6be9f076d10c708a4b28799c1ea63d0d410d73
Kenji Saito <ken-yo@mbr.nifty.com>
2023-08-11 10:24:44 +0900

    Fix the error handling

--- src/app/theaters/page.tsx
+++ src/app/theaters/page.tsx
@@ -3,24 +3,32 @@ import { useState } from 'react';
 import { useSupabase } from '../supabase';
 import { Input, Button } from '@supabase/ui';
 
+export interface Theater {
+  id: string;
+  name: string;
+}
+
 export default function Index(): JSX.Element {
   const supabase = useSupabase();
   const [name, setName] = useState<string>();
   const [theaters, setTheaters] = useState<{ id: string; name: string }[]>();
+  const [error, setError] = useState<string>();
 
-  supabase
+  void supabase
     .from('theaters')
     .select<'*', { id: string; name: string }>()
     .then(({ data }) => setTheaters(data ?? []));
 
   function handleClick() {
-    if (name && name.length > 0) {
-      supabase
-        .from('theaters')
-        .insert(name)
-        .select<'*', { id: string; name: string }>()
-        .then(({ data }) => setTheaters(data ?? []));
+    if (!name || name.length === 0) {
+      setError('Input to theater name');
+      return;
     }
+    void supabase
+      .from('theaters')
+      .insert(name)
+      .select<'*', { id: string; name: string }>()
+      .then(({ data }) => setTheaters(data ?? []));
   }
 
   return (
@@ -35,9 +43,8 @@ export default function Index(): JSX.Element {
           label="Teater name"
           placeholder="name of theater to add"
           onChange={(event) => setName(event.target.value)}
+          error={error}
         />
-      </div>
-      <div>
         <Button onClick={() => handleClick()}>Save</Button>
       </div>
     </>
