35389cf2f3ff827a0cb4cefbafdf130c3223c457
Kenji Saito <ken-yo@mbr.nifty.com>
2023-08-14 19:22:47 +0900

    Add display to title of shows

--- src/app/titles/titles.tsx
+++ src/app/titles/titles.tsx
@@ -3,6 +3,33 @@ import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 import { useEffect, useState, useCallback } from 'react';
 import { Title } from '../../types';
 
+export function useTitle(id: string) {
+  const supabase = useSupabaseClient<Title>();
+  const session = useSession();
+  const [title, setTitle] = useState<Title>();
+  const [error, setError] = useState<Error>();
+
+  useEffect(() => {
+    void supabase
+      .from('titles')
+      .select()
+      .match({ id })
+      .single<Title>()
+      .then(({ data, error }) => {
+        if (error) {
+          setError(new Error(error.message));
+        } else {
+          setTitle(data);
+        }
+      });
+  }, [supabase, session, id]);
+
+  return {
+    title,
+    error,
+  };
+}
+
 export function useTitles() {
   const supabase = useSupabaseClient<Title>();
   const session = useSession();
