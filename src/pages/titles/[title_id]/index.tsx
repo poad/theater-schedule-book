827f061925009af5b7c0f46f0afd65936d94b2d1
Kenji Saito <ken-yo@mbr.nifty.com>
2023-11-26 22:39:41 +0900

    Fix the width

--- src/pages/titles/[title_id]/index.tsx
+++ src/pages/titles/[title_id]/index.tsx
@@ -141,7 +141,7 @@ function Main({
       <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8 mb-8">
           <div className="overflow-hidden">
-            <table className="min-w-full text-left text-sm font-light">
+            <table className="min-w-full text-left text-sm font-light border-collapse">
               <thead className="border-b font-medium dark:border-neutral-500">
                 <tr>
                   <th>Viewed</th>
