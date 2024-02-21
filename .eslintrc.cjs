6284902bbc39b303d3ff3c62d20f0e9287afef6b
Kenji Saito <ken-yo@mbr.nifty.com>
2024-02-21 19:09:53 +0900

    Bumps node modules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+  ignorePatterns: ['src/mocks/*.ts'],
   root: true,
   env: {
     browser: true,
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,12 +1,14 @@
-import { rest } from 'msw';
+import { http, HttpResponse } from 'msw';
 
 const endpoint = process.env.NEXT_PUBLIC_SUPABASE_URL ?? '';
 
 export const handlers = [
-  rest.get(
-    `${endpoint}auth/v1/token?grant_type=refresh_token`,
-    (_req, res, ctx) => {
-      return res(ctx.status(404));
-    },
-  ),
+  http.get(`${endpoint}auth/v1/token?grant_type=refresh_token`, () => {
+    return new HttpResponse('Not found', {
+      status: 404,
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    });
+  }),
 ];
