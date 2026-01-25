6416598c0007ba52ec6db8c97063fea4b43b9cca
Kenji Saito <ken-yo@mbr.nifty.com>
2026-01-25 16:09:15 +0900

    Bumps node modules

--- src/feature/date-view/index.tsx
+++ src/feature/date-view/index.tsx
@@ -3,5 +3,5 @@ export const DateView = (props: {
   date: Date | number
 }) => {
   const date = typeof props.date === 'number' ? new Date(props.date) : props.date;
-  return <>{date.getFullYear()}/{date.getMonth() + 1}/{date.getDate()} ({dateTimeFormat.format(date)}) {date.getHours().toString().padStart(2, "0")}:{date.getMinutes().toString().padStart(2, "0")}</>;
+  return <>{date.getFullYear()}/{date.getMonth() + 1}/{date.getDate()} ({dateTimeFormat.format(date)}) {date.getHours().toString().padStart(2, '0')}:{date.getMinutes().toString().padStart(2, '0')}</>;
 };
