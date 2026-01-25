cda8027fbc7c7941fab14e9bb8a25f3b09293cf8
Kenji Saito <ken-yo@mbr.nifty.com>
2026-01-25 13:57:12 +0900

    Fix the errors

--- src/feature/date-view/index.tsx
+++ src/feature/date-view/index.tsx
@@ -3,5 +3,5 @@ export const DateView = (props: {
   date: Date | number
 }) => {
   const date = typeof props.date === 'number' ? new Date(props.date) : props.date;
-  return <>{date.getFullYear()}/{date.getMonth}/{date.getDate()} ({dateTimeFormat.format(date)}) {date.getHours()}:{date.getMinutes()}</>;
+  return <>{date.getFullYear()}/{date.getMonth()}/{date.getDate()} ({dateTimeFormat.format(date)}) {date.getHours()}:{date.getMinutes()}</>;
 };
